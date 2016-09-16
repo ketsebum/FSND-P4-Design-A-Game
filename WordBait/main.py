e37985eaa05d30f137ed0be7297c6ca89eb721d6
Ketse <snunleys@gmail.com>
2016-09-16 15:01:05 -0400

    Only e-mail users with active games, when it is their turn

--- WordBait/main.py
+++ WordBait/main.py
@@ -8,7 +8,7 @@ import webapp2
 from google.appengine.api import mail, app_identity
 from api import WordBaitAPI
 
-from models import User
+from models import User, Game
 
 
 class SendReminderEmail(webapp2.RequestHandler):
@@ -16,16 +16,14 @@ class SendReminderEmail(webapp2.RequestHandler):
         """Send a reminder email to each User with an email about games.
         Called every hour using a cron job"""
         app_id = app_identity.get_application_id()
-        users = User.query(User.email != None)
-        for user in users:
+        active_games = Game.query(Game.game_over == False).fetch()
+        for game in active_games:
             subject = 'This is a reminder!'
-            body = 'Hello {}, try out Guess A Number!'.format(user.name)
-            # This will send test emails, the arguments to send_mail are:
-            # from, to, subject, body
+            body = 'Hello {}, You stil have an active game going, and it\'s your turn!'.format(game.turn.get().name)
             mail.send_mail('noreply@{}.appspotmail.com'.format(app_id),
-                           user.email,
+                           game.turn.get().email,
                            subject,
-                           body)
+                           body))
 
 
 class UpdateAverageMovesRemaining(webapp2.RequestHandler):
